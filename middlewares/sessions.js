f94570ce7c3f9128105a4c4a22d7e380034796f9
shkshitiz <shkshitiz1@gmail.com>
2022-10-05 06:50:50 +1100

    added sessions "middleware"

--- middlewares/sessions.js
+++ middlewares/sessions.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const espressSession = require('express-session')
 
 const sessions = espressSession({
@@ -9,3 +10,16 @@ const sessions = espressSession({
 })
 
 module.exports = sessions
+=======
+const expressSession = require('express-session')
+
+const sessions = expressSession({
+  key: 'user_sid',
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24}
+})
+
+module.exports = sessions
+>>>>>>> de7b529 (added sessions "middleware")
