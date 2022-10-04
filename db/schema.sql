bd8a7ec30ccb5b632466376f8839bae38577fc40
Titus Huang <titus.the.huang@gmail.com>
2022-10-04 19:53:47 +1030

    Added users table in database

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ CREATE DATABASE budget_tracker_db;
 \c budget_tracker_db
 -- \password {username}
 -- don't forget to add a password in 
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    email TEXT,
+    password_digest TEXT
+);
