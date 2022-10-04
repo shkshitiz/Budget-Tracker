63ec442f1ed83a9ff0627c6f6e021652069f4265
shkshitiz <shkshitiz1@gmail.com>
2022-10-05 09:38:28 +1100

    tiny indentation fix

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,14 +7,14 @@ const User = require('../models/user')
 
 // routes
 router.post('/', (req, res) => {
-    const {username, email, password} = req.body
+  const {username, email, password} = req.body
 
-    // using bcypt to create password digest
-    const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+  // using bcypt to create password digest
+  const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
 
-    User
-        .create(username, email, passwordDigest)
-        .then(username => res.json(username))
+  User
+    .create(username, email, passwordDigest)
+    .then(username => res.json(username))
 })
 
 module.exports = router 
