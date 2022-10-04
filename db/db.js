cd91e522535f7f6a284ca2ace2b2ff45858c1ea1
Titus Huang <titus.the.huang@gmail.com>
2022-10-04 12:40:41 +1030

    Basic database file added

---
+++ db/db.js
@@ -0,0 +1,28 @@
+const pg = require('pg')
+
+// change this to your actual local database name
+const localDbName = 'budget_tracker_db'
+
+let db;
+
+if (process.env.DATABASE_URL) {
+    db = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+	})
+} else {
+    if (process.env.DB_PASSWORD) {
+        db = new pg.Pool({
+            database: localDbName,
+            password: process.env.DB_PASSWORD
+        })
+    } else {
+        db = new pg.Pool({
+            database: localDbName
+        })
+    }
+}
+
+module.exports = db
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE budget_tracker_db;
+\c budget_tracker_db
+-- \password {username}
+-- don't forget to add a password in 
