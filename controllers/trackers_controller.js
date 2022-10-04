d0ba86ad995b97ad6776b94b93d4f388bd7ccdf9
shkshitiz <shkshitiz1@gmail.com>
2022-10-05 09:38:28 +1100

    tiny indentation fix

--- controllers/trackers_controller.js
+++ controllers/trackers_controller.js
@@ -7,36 +7,50 @@ const Transaction = require('../models/transaction')
 // routes
 // read
 router.get('/', (req, res) => {
-    Transaction
-    .findAll()
-    .then(transactions => res.json(transactions))
+  Transaction
+  .findAll()
+  .then(transactions => res.json(transactions))
 })
 
 // create
 router.post('/', (req, res) => {
-    const {name, category, amount, description, period, starDate, endDate} = req.body
+  const {name, category, amount, description, period, starDate, endDate} = req.body
 
 
-    Transaction
-        .create(name, category, amount, description, period, starDate, endDate)
-        .then(transaction => res.json(transaction))
+  Transaction
+    .create(name, category, amount, description, period, starDate, endDate)
+    .then(transaction => res.json(transaction))
 })
 
 // update
 // router.put('/', (req, res) => {
 //   const transactionId = req.params.id
+router.get('/:id', (req, res) => {
+  const transactionId = req.params.id
 
+  Transaction
+    .find(transactionId)
+    .then(() => res.json({message: `grabbed ${transactionId}`}))
+})
+
+router.put('/:id', (req, res) => {
+  const {name, category, amount, description, period, starDate, endDate} = req.body
+
+  Transaction
+    .update(name, category, amount, description, period, starDate, endDate)
+
+})
 
 
 // })
 
 // delete
 router.delete('/:id', (req, res) => {
-    const transactionId = req.params.id
+  const transactionId = req.params.id
 
-    Transaction
-        .delete(transactionId)
-        .then(() => res.json({message: 'deleted successfully'}))
+  Transaction
+    .delete(transactionId)
+    .then(() => res.json({message: 'deleted successfully'}))
 })
 
 module.exports = router
