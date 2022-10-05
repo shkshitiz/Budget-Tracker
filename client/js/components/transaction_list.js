6846c8b44f96ee220839229337fe2a7bc3e35ed5
Titus Huang <titus.the.huang@gmail.com>
2022-10-06 03:11:34 +1030

    added transaction history and list to their component files

--- client/js/components/transaction_list.js
+++ client/js/components/transaction_list.js
@@ -2,7 +2,7 @@
 function renderTransactionListTitle() {
     return `
         <div class="transaction-list-title">
-            <p class="title-date">Date</p>
+            <p class="title-date">DATE</p>
             <p class="title-transaction">Transaction</p>
         </div>
     `
@@ -13,6 +13,33 @@ function renderTransactionListTitle() {
 
 function renderTransactionList(transactionData) {
     return `
+        <div class="transaction-tracking-items">
+            <div class="tracked-item">
+                <div class="tracked-item-date">20 Sep</div>
+                <span class="tracked-item-divider"></span>
+                <div class="tracked-item-info item-style-red">
+                <div class="tracked-item-title">- $300 - Rent</div>
+                <div class="tracked-item-content"></div>
+                </div>
+            </div>
 
+            <div class="tracked-item">
+                <div class="tracked-item-date">10 Sep</div>
+                <span class="tracked-item-divider"></span>
+                <div class="tracked-item-info item-style-brown">
+                <div class="tracked-item-title">- $300 - Food Shopping</div>
+                <div class="tracked-item-content"></div>
+                </div>
+            </div>
+
+            <div class="tracked-item">
+                <div class="tracked-item-date">1 Sep</div>
+                <span class="tracked-item-divider"></span>
+                <div class="tracked-item-info item-style-blue">
+                <div class="tracked-item-title">+ $1200 - Salary</div>
+                <div class="tracked-item-content"></div>
+                </div>
+            </div>
+        </div>
     `
 }
