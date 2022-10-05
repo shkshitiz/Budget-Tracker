c3bc36b405da7e4268d0a51e869274ccc1f8f2a1
Angela Li <angelalicareer@gmail.com>
2022-10-05 15:01:44 +1030

    sign_up

---
+++ client/js/components/sign_up.js
@@ -0,0 +1,41 @@
+function renderSignUp() {
+  document.querySelector('#page').innerHTML = `
+  <main>
+  <form onSubmit="createUser(event)">
+    <h2>
+      Sign Up
+    </h2>
+    <section>
+      <label for="">Username: </label>
+      <input type="text" name="username">
+    </section>
+    <section>
+      <label for="">Email: </label>
+      <input type="text" name="email">
+    </section>
+    <section>
+      <label for="">Password: </label>
+      <input type="password" name="password">
+    </section>
+    <button>Sign Up</button>
+    </form>
+   </main>
+  `
+}
+
+function createUser(event){
+  event.preventDefault()
+  const form = event.target
+  const data = Object.fromEntries(new FormData(form))
+
+  fetch('/api/users', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(userName => {
+      state.loggedInUserName = userName
+      renderRestaurantList()
+    })
+}
