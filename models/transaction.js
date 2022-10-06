a12a67f2bb202d17c59a6b8fd7c921c71a547967
Titus Huang <titus.the.huang@gmail.com>
2022-10-06 17:01:04 +1030

    updated transaction.js model file

--- models/transaction.js
+++ models/transaction.js
@@ -2,49 +2,107 @@ const db = require('../db/db')
 
 const Transaction = {
   findAllByUserId: (userId) => {
-    const sql = `SELECT * FROM transactions WHERE user_id = $1`
-    console.log("sql being ran")
+    const sql = `
+      SELECT * FROM transactions WHERE user_id = $1
+    `
+    
+    return db
+      .query(sql, [userId])
+      .then(dbRes => dbRes.rows)
+  },
+
+  create: (userId, date, amount, name, description) => {
+    const sql = `
+      INSERT INTO transactions (user_id, date, amount, name, description)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING *
+    `
+
+    return db
+            .query(sql, [userId, date, amount, name, description])
+            .then(dbRes => dbRes.rows[0])
+  },
+
+  verify: (id, userId) => {
+    // Grab user_id from transaction that you want to access
+    const sql = `SELECT user_id WHERE id = $1`
+
     return db
             .query(sql, [userId])
-            .then(dbRes => dbRes.rows)
+            .then(dbRes => {
+              if (dbRes.rows[0] === id) {
+                return true
+              } else {
+                console.log("Error: User is not the owner of this transaction!")
+                return false
+              }
+            })
   },
 
-  create: (userId, startDate, endDate, period, amount, name, description, category) => {
-    const sql = `INSERT INTO transactions (user_id, start_date, end_date, period, amount, name, description, category)
-                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-                 RETURNING *`
+  update: (id, userId, date, amount, name, description) => {
+    // this will verify if the user is the owner of the transaction or not
+    this.verify(id, userId)
+      .then(isOwnerOfTransaction => {
+        if (!isOwnerOfTransaction) {
+          return { "error": "User is not the owner of this transaction!" }
+        }
+        const sql = `
+          UPDATE transactions
+          SET
+            date = $2, amount = $3, name = $4, description = $5
+          WHERE
+            id = $1
+          RETURNING *
+        `
 
-    return db
-            .query(sql, [userId, startDate, endDate, period, amount, name, description, category])
-            .then(dbRes => dbRes.rows[0])
-  },
-
-  update: (id, startDate, endDate, period, amount, name, description, category) => {
-    const sql = `UPDATE transactions SET start_date=$1, end_date=$2, period=$3, amount=$4, name=$5, description=$6, category=$7
-                 WHERE id=$8
-                 RETURNING *`
-
-    return db
-            .query(sql, [startDate, endDate, period, amount, name, description, category, id])
-            .then(dbRes => dbRes.rows[0])
+        console.log("Transaction has been updated")
+        return db
+                .query(sql, [id, date, amount, name, description])
+                .then(dbRes => dbRes.rows[0])
+      })
   },
 
   deleteById: (id) => {
-    const sql = `DELETE FROM transactions WHERE id = $1`
-    db.query(sql, [id])
+    this.verify(id, userId)
+      .then(isOwnerOfTransaction => {
+        if (!isOwnerOfTransaction) {
+          return { "error": "User is not the owner of this transaction!" }
+        }
+
+        const sql = `
+          DELETE FROM
+            transactions
+          WHERE
+            id = $1
+        `
+
+        db.query(sql, [id])
+
+        console.log("Transaction has been deleted")
+        return { "message": "Transaction has been deleted" }
+      })
   },
 
-  filterByTime: (start, end) => {
-    const sql = `SELECT * FROM transactions WHERE start_date >= $1 AND start_date <= $2 ORDER BY start_date DESC`
-    return db
-            .query(sql, [start, end])
-            .then(dbRes => dbRes.rows)
-  },
+  findCurrentMonthByUserId: (date, userId) => {
+    const sql = `
+      SELECT *
+      FROM transactions
+      WHERE
+        user_id = $1,
+        date >= $2
+        AND
+        date <= $3
+      ORDER BY
+        date
+      DESC
+    `
+    
+    const trunDate = date.slice(0, 10)
+    let startOfMonth = new Date(trunDate.slice(0, 4), trunDate.slice(5, 7)-1, 2)
+    let endOfMonth = new Date(trunDate.slice(0, 4), trunDate.slice(5, 7), 1)
 
-  filterByCategory: (category) => {
-    const sql = `SELECT * FROM transactions WHERE category = $1 ORDER BY start_date DESC`
     return db
-            .query(sql, [category])
+            .query(sql, [userId, startOfMonth, endOfMonth])
             .then(dbRes => dbRes.rows)
   }
 }
