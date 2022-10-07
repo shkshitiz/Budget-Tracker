493dbdf546638f890a8f661814204a285a82d741
Titus Huang <titus.the.huang@gmail.com>
2022-10-07 12:18:23 +1030

    mobile nav does not show up when not logged in anymore

--- client/js/components/mobile_nav.js
+++ client/js/components/mobile_nav.js
@@ -2,8 +2,17 @@
 
 function renderMobileNavMenu() {
   if (typeof loggedInUserEmail === 'string') {
+    document.querySelector('#mobile-nav').classList.add('sticky-nav')
     document.querySelector('#mobile-nav').innerHTML = `
-
+      <div class="container-fluid">
+        <div class="col navbar-text sticky-nav-text" onclick="renderTransactionOverview()">Overview</div>
+        <div class="col navbar-text sticky-nav-text" onclick="renderTransactionManager()">Manager</div>
+        <div class="col navbar-text sticky-nav-text" onclick="renderTransactionHistory()">History</div>
+      </div>
     `
+  } else {
+    if (document.querySelector('#mobile-nav').classList.contains('sticky-nav')) {
+      document.querySelector('#mobile-nav').classList.remove('sticky-nav')
+    }
   }
 }
