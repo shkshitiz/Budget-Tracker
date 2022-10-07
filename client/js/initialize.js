1418cea57cc6c3ac4e261bd903656b6d39d28f87
shkshitiz <shkshitiz1@gmail.com>
2022-10-07 11:57:54 +1030

    woking overview

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -15,7 +15,6 @@ fetch('/api/sessions')
 
 if (typeof state.loggedInUserEmail === 'string') {
   let data = `{ "userEmail" : "${state.loggedInUserEmail}" }`
-  console.log(data)
 
   fetch('/api/transactions/user', {
     method: 'POST',
@@ -25,6 +24,5 @@ if (typeof state.loggedInUserEmail === 'string') {
     .then(res => res.json())
     .then(transactions => {
       state.userTransactions = transactions
-      console.log(transactions)
     })
 }
