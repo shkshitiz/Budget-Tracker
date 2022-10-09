f5026334f8c923488ac68b72a4291e13446d0760
Titus Huang <titus.the.huang@gmail.com>
2022-10-10 00:12:09 +1030

    converted login/signup forms to bootstrap

--- client/js/components/user/login.js
+++ client/js/components/user/login.js
@@ -2,14 +2,20 @@ function renderLogin() {
   document.querySelector('#page').innerHTML = `
   <div class="center-dialog d-flex align-items-center justify-content-center">
     <div class="card-body">
-      <section class='main-logo'> 
-      <img src="https://i.imgur.com/1aor9pi.png" alt="">
-      <h3>Login</h3>
-      </section>
+      <div class='main-logo'> 
+        <img src="https://i.imgur.com/1aor9pi.png" alt="">
+        <h3>Login</h3>
+      </div>
       <div class="log-in-form">
-        <form onSubmit="login(event)">
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
+        <form onSubmit="login(event)" class="form-control-sm">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" name="email">
+            <label for="emailInput">Email address</label>
+          </div>
+          <div class="form-floating mb-5">
+            <input type="password" class="form-control" id="passwordInput" placeholder="password" name="password">
+            <label for="passwordInput">Password</label>
+          </div>
           <button class="btn btn-primary">Log in</button>
         </form>
       </div>
--- client/js/components/user/sign_up.js
+++ client/js/components/user/sign_up.js
@@ -2,15 +2,24 @@ function renderSignUp() {
   document.querySelector('#page').innerHTML = `
   <div class="center-dialog d-flex align-items-center justify-content-center">
     <div class="card-body">
-      <section class='main-logo'> 
+      <div class='main-logo'> 
         <img src="https://i.imgur.com/1aor9pi.png" alt="">
         <h3>Sign Up</h3>
-      </section>
+      </div>
       <div class="sign-up-form">
-        <form onSubmit="createUser(event)">
-          <input type="text" placeholder="Name" name="username" />
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
+        <form onSubmit="createUser(event)" class="form-control-sm">
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="usernameInput" placeholder="Username" name="username">
+            <label for="usernameInput">Username</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="emailInput" placeholder="name@example.com" name="email">
+            <label for="emailInput">Email address</label>
+          </div>
+          <div class="form-floating mb-5">
+            <input type="password" class="form-control" id="passwordInput" placeholder="password" name="password">
+            <label for="passwordInput">Password</label>
+          </div>
           <button class="btn btn-primary">Sign Up</button>
         </form>
       </div>
